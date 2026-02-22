5a96b0623dbfac7d260f32c5b15e4e4ab5ee866b
Rendani Ramagoma <codewithrendani@gmail.com>
2026-02-22 13:27:29 +0200

    Replace favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import WhatsAppFloating from "../components/ui/WhatsAppFloating";
-
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Expressmaids",
@@ -28,17 +14,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <WhatsAppFloating />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
